bbdcf9e6b73913d286c5f734bd7e40cdb486d38b
Franco Caldas <francocaldas@gmail.com>
2021-12-17 08:27:43 -0300

    mostra a data e hora do comentário

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,10 @@
 class Comment < ApplicationRecord
   belongs_to :profile
   belongs_to :headhunter
+
+  def headhunter_email
+    @headhunter = Headhunter.find_by(id: headhunter_id)
+    "#{@headhunter.email}"
+  end
+  
 end
